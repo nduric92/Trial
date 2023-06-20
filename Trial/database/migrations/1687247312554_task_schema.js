0960ebafd4952581b71d7b89075eea9be2406641
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-06-20 11:15:49 +0200

    migrations - basic

---
+++ Trial/database/migrations/1687247312554_task_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TaskSchema extends Schema {
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      table.string('name')
+      table.text('description')
+      table.integer('project_id').references('project.id')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+}
+
+module.exports = TaskSchema
